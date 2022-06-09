73e22c762f3bec829ff023f6997a02b8243cd2ed
Andrei Kuleuski <kuljok@gmail.com>
2022-06-10 00:41:18 +0300

    Initital Treasure Hunters server commit

---
+++ NtlStudio.TreasureHunters.Server/NtlStudio.TreasureHunters.Model/GameState.cs
@@ -0,0 +1,11 @@
+﻿namespace NtlStudio.TreasureHunters.Game;
+
+public class GameState
+{
+    public GameState(Guid gameId)
+    {
+        GameId = gameId;
+    }
+
+    public Guid GameId { get; }
+}
