b1a7b593bf1182f5e88a1d23f70a8448e506f501
Andrei Kuleuski <kuljok@gmail.com>
2022-07-04 08:25:58 +0300

    Add Create Game endpoint. Update the routes for both methods.

---
+++ NtlStudio.TreasureHunters.Server/NtlStudio.TreasureHunters.WebServer/Controllers/GamesController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using NtlStudio.TreasureHunters.Game;
+
+namespace NtlStudio.TreasureHunters.WebServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GamesController : ControllerBase
+{
+    private readonly ILogger<GamesController> _logger;
+    
+    public GamesController(ILogger<GamesController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpPost]
+    [Route("[action]")]
+    public async Task<IActionResult> Create()
+    {
+        var game = new GameState(Guid.NewGuid());
+        _logger.LogInformation($"A new game {game.Id} has been created.");
+        return CreatedAtAction(nameof(Get), new {Id = game.Id}, game);
+    }
+
+    [HttpGet]
+    [Route("{Id}")]
+    public GameState Get(Guid id)
+    {
+        _logger.LogInformation($"Game {id} has been requested.");
+        // returns hardcoded gamestate yet
+        return new GameState(id);
+    }
+}
